e83ae227de784ed67986d98f0c95e84a63ff6b1f
leovam <leovam171@gmail.com>
2020-11-08 03:12:08 -0500

    update 20201108

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python jemdoc.py courses.jemdoc 
+python jemdoc.py services.jemdoc 
+python jemdoc.py work.jemdoc 
+python jemdoc.py teaching.jemdoc 
+python jemdoc.py publications.jemdoc 
+python jemdoc.py Misc.jemdoc 
+python jemdoc.py MENU
+python jemdoc.py index.jemdoc 
